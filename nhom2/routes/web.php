c670ce46c5539487c6b36ae2a41fcc2c6345ac19
toankhung <lyminhtoan2004@gmail.com>
2025-03-18 13:46:09 +0700

    cắt giao diện client

--- nhom2/routes/web.php
+++ nhom2/routes/web.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
 });
 Route::get('/contact', function () {
     return view('client.pages.contact');
-    return view('admin.layouts.master');
-});
-Route::get('/product-add', function () {
-    return view('admin.pages.product.add');
 });
 Route::get('/shop', function () {
     return view('client.pages.shop');
