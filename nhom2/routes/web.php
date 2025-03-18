619e128be3de2df72a8a7b7e3b1404e5a9d925f8
toankhung <lyminhtoan2004@gmail.com>
2025-03-18 13:46:09 +0700

    cắt giao diện client

--- nhom2/routes/web.php
+++ nhom2/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.pages.home');
+});
+Route::get('/contact', function () {
+    return view('client.pages.contact');
+});
+Route::get('/shop', function () {
+    return view('client.pages.shop');
 });
