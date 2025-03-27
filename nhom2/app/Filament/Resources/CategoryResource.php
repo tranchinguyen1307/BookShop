3ae38be1d0f594f318a1253582d5c70d0eee1f9f
toankhung <lyminhtoan2004@gmail.com>
2025-03-27 12:40:06 +0700

    CRUD sản phẩm

---
+++ nhom2/app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+    protected static ?string $label = 'Danh mục';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Tên danh mục')
+                    ->rules(['required']),
+
+                RichEditor::make('description')
+                    ->label('Mô tả danh mục')
+                    ->rules(['required']),
+
+                Fileupload::make('thumbnail')
+                    ->directory('categories')
+                    ->required()
+                    ->label('Hình ảnh danh mục')
+            ])
+            ->columns(1);;
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('description')
+                    ->html(),
+                ImageColumn::make('thumbnail')
+                    ->label('Hình ảnh'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ nhom2/app/Models/category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+
+class Category extends Model
+{
+      /** @use HasFactory<\Database\Factories\UserFactory> */
+      use HasFactory, Notifiable;
+    protected $fillable =
+    [
+        'name',
+        'description',
+        'status',
+        'thumbnail'
+    ];
+}
