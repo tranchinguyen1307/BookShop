f9ec75741f12c570287c4a932a239c2769a88eb3
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-03-30 19:22:54 +0700

    Chức năng đăng nhập, đăng kí, đăng xuất, quên mk, đổimk, xóa tk, cập nhật thông tin

--- nhom2/resources/views/client/layouts/master.blade.php
+++ nhom2/resources/views/client/layouts/master.blade.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>@yield('title', 'MultiShop - Online Shop Website')</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="Free HTML Templates" name="keywords">
-        <meta content="Free HTML Templates" name="description">
 
-        <!-- Favicon -->
-        <link href="{{ asset('client/img/favicon.ico') }}" rel="icon">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title', 'MultiShop - Online Shop Website')</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- Favicon -->
+    <link href="{{ asset('client/img/favicon.ico') }}" rel="icon">
 
-        <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
-        <!-- Libraries Stylesheet -->
-        <link href="{{ asset('client/lib/animate/animate.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('client/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/5.2.6/js/fileinput.min.js"></script>
 
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="{{ asset('client/css/style.css') }}" rel="stylesheet">
-        @stack('styles')
-    </head>
-    <body>
-       <x-client.navbar/>
-        @yield('content')
-        <x-client.footer/>
-    </body>
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('client/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('client/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('client/css/style.css') }}" rel="stylesheet">
+    @stack('style')
+</head>
+
+<body>
+    <x-client.navbar />
+    @yield('content')
+    <x-client.footer />
+</body>
+
 </html>
