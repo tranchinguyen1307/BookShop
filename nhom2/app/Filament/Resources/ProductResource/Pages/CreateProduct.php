34b5f351ee3044e801fad1259c5daf658b6100b1
toankhung <lyminhtoan2004@gmail.com>
2025-03-31 19:18:54 +0700

    Fix ảnh product

--- nhom2/app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ nhom2/app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -3,10 +3,27 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $record = static::getModel()::create($data);
+
+        if (!empty($data['images'])) {
+            foreach ($data['images'] as $image) {
+                $record->images()->create([
+                    'image' => $image, 
+                ]);
+            }
+        }
+
+        return $record;
+    }
 }
+
+
