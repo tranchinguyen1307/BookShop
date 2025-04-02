24dbfc9420973c4f0e4a4393ddb6b2b73e433539
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-04-02 20:26:07 +0700

    thêm bảng role và chức năng CRUD địa chỉ của người dùng

--- nhom2/app/Http/Controllers/client/UserController.php
+++ nhom2/app/Http/Controllers/client/UserController.php
@@ -112,7 +112,6 @@ class UserController extends Controller
         return redirect('/')->with('success', 'Tài khoản đã bị xóa.');
     }
 
-
     public function confirmDelete()
     {
         return view('client.pages.confirm-delete');
