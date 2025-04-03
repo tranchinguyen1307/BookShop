06740b739377726b9cb14a8ae74ac10d654905c4
HIND3110 <daodinhhay@gmail.com>
2025-04-03 13:28:15 +0700

    đổ dữ liệu sản phẩm
    # Conflicts:
    #	nhom2/app/Http/Controllers/Client/ShopController.php
    #	nhom2/resources/views/client/pages/shop.blade.php
    #	nhom2/routes/web.php

--- nhom2/resources/views/client/pages/shop.blade.php
+++ nhom2/resources/views/client/pages/shop.blade.php
@@ -47,7 +47,7 @@
                     </form>
 
                 </div>
-                
+
                 <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Lọc theo
                         danh mục</span></h5>
                 <div class="bg-light p-4 mb-30">
--- nhom2/routes/web.php
+++ nhom2/routes/web.php
@@ -32,12 +32,10 @@ Route::middleware('auth')->prefix('checkout')->name('checkout.')->controller(Che
     Route::get('/', 'process')->name('index');
     Route::post('/', 'process')->name('process');
     Route::post('/store', 'storeOrder')->name('store');
-    Route::get('/cart', [CartController::class, 'index'])->name('cart.index')->middleware('auth');
-    Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add')->middleware('auth');
-    Route::post('/cart/update', [CartController::class, 'updateCart'])->name('cart.update');
-    Route::post('/cart/remove', [CartController::class, 'removeFromCart'])->name('cart.remove');
-});
+    Route::get('/thanks', 'thanks');
 
+});
+Route::post('checkout/payment-ipn', [CheckoutController::class,'handleIPN']);
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
