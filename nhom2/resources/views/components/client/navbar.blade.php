f41af6ea2e4709bddfdc1084d689b68375c63f8d
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-04-03 12:20:33 +0700

    merge lai code trang navbar

--- nhom2/resources/views/components/client/navbar.blade.php
+++ nhom2/resources/views/components/client/navbar.blade.php
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 <!-- Topbar Start -->
 <div class="container-fluid">
-=======
- <!-- Topbar Start -->
- <div class="container-fluid">
->>>>>>> 619e128 (cắt giao diện client)
     <div class="row bg-secondary py-1 px-xl-5">
         <div class="col-lg-6 d-none d-lg-block">
             <div class="d-inline-flex align-items-center h-100">
                 <a class="text-body mr-3" href="">Giới thiệu</a>
-<<<<<<< HEAD
                 <a class="text-body mr-3" href="{{ route('contact')}}">Liên Hệ</a>
-=======
-                <a class="text-body mr-3" href="">Liên Hệ</a>
->>>>>>> 619e128 (cắt giao diện client)
                 <a class="text-body mr-3" href="">Trợ Giúp</a>
                 <a class="text-body mr-3" href="">FAQs</a>
             </div>
@@ -21,7 +12,6 @@
         <div class="col-lg-6 text-center text-lg-right">
             <div class="d-inline-flex align-items-center">
                 <div class="btn-group">
-<<<<<<< HEAD
                     <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">
                         {{ auth()->check() ? auth()->user()->email : 'Tài Khoản' }}
                     </button>
@@ -42,16 +32,6 @@
                 {{-- <div class="btn-group mx-2">
                     <button type="button" class="btn btn-sm btn-light dropdown-toggle"
                         data-toggle="dropdown">USD</button>
-=======
-                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Tài Khoản</button>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <button class="dropdown-item" type="button">Đăng nhập</button>
-                        <button class="dropdown-item" type="button">Đăng ký</button>
-                    </div>
-                </div>
-                {{-- <div class="btn-group mx-2">
-                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">USD</button>
->>>>>>> 619e128 (cắt giao diện client)
                     <div class="dropdown-menu dropdown-menu-right">
                         <button class="dropdown-item" type="button">EUR</button>
                         <button class="dropdown-item" type="button">GBP</button>
@@ -59,12 +39,8 @@
                     </div>
                 </div> --}}
                 {{-- <div class="btn-group">
-<<<<<<< HEAD
                     <button type="button" class="btn btn-sm btn-light dropdown-toggle"
                         data-toggle="dropdown">EN</button>
-=======
-                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">EN</button>
->>>>>>> 619e128 (cắt giao diện client)
                     <div class="dropdown-menu dropdown-menu-right">
                         <button class="dropdown-item" type="button">FR</button>
                         <button class="dropdown-item" type="button">AR</button>
@@ -75,7 +51,6 @@
             <div class="d-inline-flex align-items-center d-block d-lg-none">
                 <a href="" class="btn px-0 ml-2">
                     <i class="fas fa-heart text-dark"></i>
-<<<<<<< HEAD
                     <span class="badge text-dark border border-dark rounded-circle"
                         style="padding-bottom: 2px;">0</span>
                 </a>
@@ -83,13 +58,6 @@
                     <i class="fas fa-shopping-cart text-dark"></i>
                     <span class="badge text-dark border border-dark rounded-circle"
                         style="padding-bottom: 2px;">0</span>
-=======
-                    <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
-                </a>
-                <a href="" class="btn px-0 ml-2">
-                    <i class="fas fa-shopping-cart text-dark"></i>
-                    <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
->>>>>>> 619e128 (cắt giao diện client)
                 </a>
             </div>
         </div>
@@ -126,7 +94,6 @@
 <div class="container-fluid bg-dark mb-30">
     <div class="row px-xl-5">
         <div class="col-lg-3 d-none d-lg-block">
-<<<<<<< HEAD
             <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse"
                 href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
                 <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Danh mục</h6>
@@ -138,16 +105,6 @@
                     <div class="nav-item dropdown dropright">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dresses <i
                                 class="fa fa-angle-right float-right mt-1"></i></a>
-=======
-            <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
-                <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Danh mục</h6>
-                <i class="fa fa-angle-down text-dark"></i>
-            </a>
-            <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
-                <div class="navbar-nav w-100">
-                    <div class="nav-item dropdown dropright">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dresses <i class="fa fa-angle-right float-right mt-1"></i></a>
->>>>>>> 619e128 (cắt giao diện client)
                         <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
                             <a href="" class="dropdown-item">Men's Dresses</a>
                             <a href="" class="dropdown-item">Women's Dresses</a>
@@ -177,33 +134,21 @@
                 </button>
                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                     <div class="navbar-nav mr-auto py-0">
-<<<<<<< HEAD
                         <a href="{{ route('home')}}" class="nav-item nav-link active">Trang chủ</a>
                         <a href="{{ route('shop')}}" class="nav-item nav-link">Cửa hàng</a>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Bài viết <i
                                     class="fa fa-angle-down mt-1"></i></a>
-=======
-                        <a href="index.html" class="nav-item nav-link active">Trang chủ</a>
-                        <a href="shop.html" class="nav-item nav-link">Cửa hàng</a>
-                        <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Bài viết <i class="fa fa-angle-down mt-1"></i></a>
->>>>>>> 619e128 (cắt giao diện client)
                             <div class="dropdown-menu bg-primary rounded-0 border-0 m-0">
                                 <a href="cart.html" class="dropdown-item">a</a>
                                 <a href="checkout.html" class="dropdown-item">b</a>
                             </div>
                         </div>
-<<<<<<< HEAD
                         <a href="{{ route('contact')}}" class="nav-item nav-link">Liên hệ</a>
-=======
-                        <a href="contact.html" class="nav-item nav-link">Liên hệ</a>
->>>>>>> 619e128 (cắt giao diện client)
                     </div>
                     <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
                         <a href="" class="btn px-0">
                             <i class="fas fa-heart text-primary"></i>
-<<<<<<< HEAD
                             <span class="badge text-secondary border border-secondary rounded-circle"
                                 style="padding-bottom: 2px;">0</span>
                         </a>
@@ -211,13 +156,6 @@
                             <i class="fas fa-shopping-cart text-primary"></i>
                             <span class="badge text-secondary border border-secondary rounded-circle" id="cart-count"
                                 style="padding-bottom: 2px;">{{$cartCount}}</span>
-=======
-                            <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
-                        </a>
-                        <a href="" class="btn px-0 ml-3">
-                            <i class="fas fa-shopping-cart text-primary"></i>
-                            <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
->>>>>>> 619e128 (cắt giao diện client)
                         </a>
                     </div>
                 </div>
@@ -225,8 +163,4 @@
         </div>
     </div>
 </div>
-<<<<<<< HEAD
 <!-- Navbar End -->
-=======
-<!-- Navbar End -->
->>>>>>> 619e128 (cắt giao diện client)
