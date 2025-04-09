266afb5055e7c6f5d52b7adb7559722f5fc2c904
toankhung <lyminhtoan2004@gmail.com>
2025-04-09 14:44:48 +0700

    merge code nhánh minhtoan

--- nhom2/app/Providers/Filament/AdminPanelProvider.php
+++ nhom2/app/Providers/Filament/AdminPanelProvider.php
@@ -36,9 +36,13 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
+                // Widgets\AccountWidget::class,
+                \App\Filament\Widgets\StatsOverview::class, 
+                \App\Filament\Widgets\UserChart::class,
+                \App\Filament\Widgets\ProductChart::class,
+                // \App\Filament\Widgets\ProductCategoryChart::class,
 
             ])
             ->middleware([
