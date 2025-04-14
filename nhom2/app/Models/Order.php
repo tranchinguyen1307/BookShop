5090db6886fc78e6a5ee7b72feb777b91a7dc812
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-04-14 14:19:05 +0700

    thêm cột phone và lý do hủy đơn vào model Order

--- nhom2/app/Models/Order.php
+++ nhom2/app/Models/Order.php
@@ -14,9 +14,11 @@ class Order extends Model
         'total_price',
         'shipping_fee',
         'address',
+        'phone',
         'payment_method',
         'status',
-        'order_code'
+        'order_code',
+        'cancellation_reason'
     ];
 
     public function user()
