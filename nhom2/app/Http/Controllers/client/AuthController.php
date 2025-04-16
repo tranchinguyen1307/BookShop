3a13fd412334086c1aa5919ebea283e3f240a39c
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-04-16 19:00:22 +0700

    xóa chú thích

--- nhom2/app/Http/Controllers/client/AuthController.php
+++ nhom2/app/Http/Controllers/client/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role_id' => 1 // Giả sử 2 là role khách hàng
+            'role_id' => 1
         ]);
 
         Auth::login($user);
