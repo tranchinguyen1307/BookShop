0cd4f2503c21e7cdcb42f41c0d0cf3df12125d5a
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2025-04-16 21:50:23 +0700

    thêm bắt lỗi form cho địa chỉ

--- nhom2/resources/views/client/pages/addresses/create.blade.php
+++ nhom2/resources/views/client/pages/addresses/create.blade.php
@@ -8,29 +8,41 @@
 
             <div class="mb-3">
                 <label for="province" class="form-label">Tỉnh/Thành phố:</label>
-                <select name="province" id="province" class="form-control" required>
+                <select name="province" id="province" class="form-control">
                     <option value="">Chọn tỉnh/thành phố</option>
                 </select>
+                @error('province')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="district" class="form-label">Quận/Huyện:</label>
-                <select name="district" id="district" class="form-control" required>
+                <select name="district" id="district" class="form-control">
                     <option value="">Chọn quận/huyện</option>
                 </select>
+                @error('district')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="ward" class="form-label">Phường/Xã:</label>
-                <select name="ward" id="ward" class="form-control" required>
+                <select name="ward" id="ward" class="form-control">
                     <option value="">Chọn phường/xã</option>
                 </select>
+                @error('ward')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="detail_address" class="form-label">Địa chỉ chi tiết:</label>
                 <input type="text" name="detail_address" id="detail_address" class="form-control"
-                    placeholder="Số nhà, tên đường" required>
+                    placeholder="Số nhà, tên đường" value="{{ old('detail_address') }}">
+                @error('detail_address')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
 
             {{-- Input ẩn để lưu địa chỉ hoàn chỉnh --}}
@@ -38,6 +50,7 @@
 
             <button type="submit" class="btn btn-success">Lưu</button>
         </form>
+
     </div>
     @push('scripts')
 
