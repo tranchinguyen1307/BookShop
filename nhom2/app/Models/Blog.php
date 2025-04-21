2987121693ee4e217f3b7e44f5130105e27a26fc
HIND3110 <daodinhhay@gmail.com>
2025-04-21 19:27:30 +0700

    CRUD bình luận

--- nhom2/app/Models/Blog.php
+++ nhom2/app/Models/Blog.php
@@ -27,5 +27,10 @@ class Blog extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
 
